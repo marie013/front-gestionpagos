d41360d6307170475912294256af92c23f58548b
Sofia Ruiz <sofiruiz4502@gmail.com>
2024-11-21 18:43:38 -0300

    realizo modificaciones en el registro de factura, con cuit del cliente y se trabaja con las cookies

---
+++ src/components/cookies.js
@@ -0,0 +1,16 @@
+function obtenerCookie(nombre) {
+    const name = `${nombre}=`;
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i].trim();
+      if (c.indexOf(name) === 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  export {
+    obtenerCookie
+  }
